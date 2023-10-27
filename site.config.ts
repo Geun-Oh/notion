139a1714f0f6ec64726db1788e3d032e02594dd7
Geun-Oh <kandy1002@naver.com>
2023-10-28 02:47:13 +0900

    config: favicon configuration

--- site.config.ts
+++ site.config.ts
@@ -9,12 +9,12 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: "Geun-Oh's Personal Docs",
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: "Geun-Oh's Personal Docs",
 
   // social usernames (optional)
   // twitter: 'transitive_bs',

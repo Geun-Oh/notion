e1294060ee4639b7b26b77e6d786ef860303795a
Geun-Oh <kandy1002@naver.com>
2023-11-10 12:52:55 +0900

    feat: add blog rediect page

---
+++ pages/blog/use-funnel.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export default function Page() {
+  window.location.href =
+    'https://velog.io/@kandy1002/%EC%88%98%EC%A0%9C-useFunnel%EB%A1%9C-%ED%8D%BC%EB%84%90-%EB%8D%B0%EC%9D%B4%ED%84%B0-%EC%9D%80%EB%8B%89%ED%99%94%ED%95%98%EA%B8%B0'
+  return <></>
+}

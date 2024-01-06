e30c9163e4c1569b27b589b61da671d159f0a697
Geun-Oh <kandy1002@naver.com>
2024-01-06 18:23:31 +0100

    feat: make custom header breadcrumb

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -4,12 +4,14 @@ import * as types from 'notion-types'
 import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
 import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
 import cs from 'classnames'
-import { Breadcrumbs, Header, Search, useNotionContext } from 'react-notion-x'
+import { Breadcrumbs, Search, useNotionContext } from 'react-notion-x'
 
-import { isSearchEnabled, navigationLinks, navigationStyle } from '@/lib/config'
+import { isSearchEnabled, navigationLinks } from '@/lib/config'
 import { useDarkMode } from '@/lib/use-dark-mode'
 
 import styles from './styles.module.css'
+
+const ROOT_PAGE_ID = 'c40a54e2-42ae-4fbd-a373-2ff9df24b8d4'
 
 const ToggleThemeButton = () => {
   const [hasMounted, setHasMounted] = React.useState(false)
@@ -38,14 +40,48 @@ export const NotionPageHeader: React.FC<{
 }> = ({ block }) => {
   const { components, mapPageUrl } = useNotionContext()
 
-  if (navigationStyle === 'default') {
-    return <Header block={block} />
-  }
+  // if (navigationStyle === 'default') {
+  //   return <Header block={block} />
+  // }
 
   return (
     <header className='notion-header'>
       <div className='notion-nav-header'>
-        <Breadcrumbs block={block} rootOnly={true} />
+        <div className='notion-custom-breadcrumb'>
+          {' '}
+          <Breadcrumbs block={block} rootOnly={true} />
+          {block.id !== ROOT_PAGE_ID && (
+            <>
+              <span className='spacer'>/</span>
+              {block.parent_id !== ROOT_PAGE_ID && (
+                <>
+                  <components.PageLink
+                    href={mapPageUrl(block.parent_id)}
+                    className={cs(styles.navLink, 'title')}
+                  >
+                    {'...'}
+                  </components.PageLink>
+                  <span className='spacer'>/</span>
+                </>
+              )}
+
+              <span
+                role='img'
+                aria-label={block.format.page_icon}
+                className={cs(
+                  'icon',
+                  'notion-page-icon',
+                  'notion-breadcrumb-icon'
+                )}
+              >
+                {block.format.page_icon}
+              </span>
+              <span className={cs('title', 'notion-nav-header-active')}>
+                {block.properties.title[0][0]}
+              </span>
+            </>
+          )}
+        </div>
 
         <div className='notion-nav-header-rhs breadcrumbs'>
           {navigationLinks

42bcafc7eb9b0c3a8b59add54ed9bb29c4a4a1cb
Geun-Oh <kandy1002@naver.com>
2024-02-01 04:00:50 +0900

    feat: add cv page

---
+++ pages/cv.tsx
@@ -0,0 +1,8 @@
+import { useEffect } from 'react'
+
+export default function Page() {
+  useEffect(() => {
+    window.location.href =
+      'https://patch-strand-bb8.notion.site/FE-Developer-9b7c9a4aa54640529403d0f05f20910e?pvs=4'
+  }, [])
+}
